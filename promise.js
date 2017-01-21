7c610efaa081d266f4ea5445cf85e7009a8c980c
PLDaily <pcd12321@gmail.com>
2017-01-21 16:14:54 +0800

    数据安全性检查

--- promise.js
+++ promise.js
@@ -1,5 +1,12 @@
 ;(function(window, undefined) {
+	var isFunction = function(obj) {
+		return typeof obj == 'function';
+	}
+
 	function Promise(resolver) {
+		if (!isFunction(resolver)) {
+		    throw new TypeError('You must pass a resolver function as the first argument to the promise constructor');
+		}
 		var _this = this;
 		this.type = 'ok';//ok为resolve；err为reject
 		this.resolveArr = [];
@@ -20,34 +27,32 @@
 	}
 
 	Promise.prototype.next = function(value, type) {
-		if(type == 'ok') {
-			var fn = this.resolveArr.shift();
-		}else {
-			var fn = this.rejectArr.shift();
-		}
+		var fn = type == ok ? this.resolveArr.shift() : this.rejectArr.shift();
 		fn&&fn(value);
 	}
 
 	Promise.prototype.then = function(thenResolve, thenReject) {
 		var _this = this;
+		if (!isFunction(thenResolve)) {
+		    throw new TypeError('You must pass a thenResolver function as the first argument to the promise constructor');
+		}
+		if (!isFunction(thenReject)) {
+		    throw new TypeError('You must pass a thenReject function as the first argument to the promise constructor');
+		}
 		Promise(function(resolve, reject) {
 			function callback(value) {
 				var ret = thenResolve(value);
-				if(ret) {
+				if(typeof ret != 'undefined') {
 					resolve.call(this, ret);				
 				}
 			}
 			function errback(value) {
 				var ret = thenReject(value);
-				if(ret) {
+				if(typeof ret != 'undefined') {
 					reject.call(this, ret);
 				}
 			}
-			if(_this.type == 'ok') {
-				_this.resolveArr.push(callback);
-			}else {
-				_this.rejectArr.push(errback);
-			}
+			_this.type == 'ok' ? _this.resolveArr.push(callback) : _this.rejectArr.push(errback)
 		});
 		return this;
 	}
